cbac46fb71be8500d165653969bd799e99c4bce5
Pritesh Durgapal <priteshdurgapal@Priteshs-MacBook-Air.local>
2019-10-17 00:21:21 +0530

    added mat expansion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyTableComponent } from './my-table/my-table.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  
+  MatButtonModule,
+  
+  
+  MatInputModule
+} from '@angular/material';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MyTableComponent
+    MyTableComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    MatButtonModule,
+    MatExpansionModule,
+    MatInputModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
